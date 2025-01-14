b8d3f181f5cf8ddd6fe1f68840fe3770f3282e6f
Myusol <qoralth25@gmail.com>
2025-01-14 09:55:01 +0900

    myusol

--- app/component/Nav/Nav.tsx
+++ app/component/Nav/Nav.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRouter } from 'next/navigation';
+import { useRouter, usePathname } from 'next/navigation'; // usePathname 훅 사용
 import Link from 'next/link';
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -9,51 +9,53 @@ import { NavContainer, NavItemsContainer, LoginContainer, NavItem, Logo, LoginBu
 import Image from 'next/image';
 
 const Nav = () => {
+    const pathname = usePathname(); // 현재 경로 가져오기
     const router = useRouter();
-    const [activeTab, setActiveTab] = useState('home'); // 현재 활성화된 탭
     const [isLoggedIn, setIsLoggedIn] = useState(false); // 로그인 상태
+
+    const isActive = (paths: string[]) => paths.includes(pathname);
 
     return (
         <NavContainer>
-    {/* 로고 */}
-    <Logo>
-        <Link href="/">
-            <Image src="/logo.png" alt="로고" width={50} height={50} priority />
-        </Link>
-    </Logo>
+            {/* 로고 */}
+            <Logo>
+                <Link href="/">
+                    <Image src="/logo.png" alt="로고" width={50} height={50} priority />
+                </Link>
+            </Logo>
 
-    {/* 네비게이션 아이템 */}
-        <NavItemsContainer>
-            <NavItem active={activeTab === 'home'} onClick={() => setActiveTab('home')}>
-            <Link href="/">홈</Link>
-            </NavItem>
-            <NavItem active={activeTab === 'contract'} onClick={() => setActiveTab('contract')}>
-            <Link href="/manage_cont">계약서 관리</Link>
-            </NavItem>
-            <NavItem active={activeTab === 'archive'} onClick={() => setActiveTab('archive')}>
-            <Link href="/archive">자료실</Link>
-            </NavItem>
-            <NavItem active={activeTab === 'notice'} onClick={() => setActiveTab('notice')}>
-            <Link href="/notice">공지사항</Link>
-            </NavItem>
-            <NavItem active={activeTab === 'qna'} onClick={() => setActiveTab('qna')}>
-            <Link href="/faq">Q&A</Link>
-            </NavItem>
-        </NavItemsContainer>
-        {/* 로그인 / 유저 정보 */}
-        <LoginContainer>
-            {isLoggedIn ? (
-            <UserInfo>
-                {/* 아이콘 */}
-                <span onClick={() => router.push('/mypage')} style={{ cursor: 'pointer', fontSize: '1.6em'}}>
-                <FontAwesomeIcon icon={faCircleUser} size="lg" />
-                </span>
-                <button onClick={() => setIsLoggedIn(false)}>로그아웃</button>
-            </UserInfo>
-            ) : (
-            <LoginButton onClick={() => setIsLoggedIn(true)}>로그인</LoginButton>
-            )}
-        </LoginContainer>
+            {/* 네비게이션 아이템 */}
+            <NavItemsContainer>
+                <NavItem active={pathname === '/'}>
+                    <Link href="/">홈</Link>
+                </NavItem>
+                <NavItem active={pathname === '/manage_cont'}>
+                    <Link href="/manage_cont">계약서 관리</Link>
+                </NavItem>
+                <NavItem active={isActive(['/archive','/law'])}>
+                    <Link href="/archive">자료실</Link>
+                </NavItem>
+                <NavItem active={pathname === '/notice'}>
+                    <Link href="/notice">공지사항</Link>
+                </NavItem>
+                <NavItem active={isActive(['/faq', '/qna'])}>
+                    <Link href="/faq">Q&A</Link>
+                </NavItem>
+            </NavItemsContainer>
+
+            {/* 로그인 / 유저 정보 */}
+            <LoginContainer>
+                {isLoggedIn ? (
+                    <UserInfo>
+                        <span onClick={() => router.push('/mypage')} style={{ cursor: 'pointer', fontSize: '1.6em' }}>
+                            <FontAwesomeIcon icon={faCircleUser} size="lg" />
+                        </span>
+                        <button onClick={() => setIsLoggedIn(false)}>로그아웃</button>
+                    </UserInfo>
+                ) : (
+                    <LoginButton onClick={() => setIsLoggedIn(true)}>로그인</LoginButton>
+                )}
+            </LoginContainer>
         </NavContainer>
     );
 };
