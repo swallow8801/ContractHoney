46dc31531accaa58570b8a0af003c0775867d23b
pince3120 <67889468+pince3120@users.noreply.github.com>
2025-01-20 13:45:16 +0900

    Update database.tsx

--- app/lib/database.tsx
+++ app/lib/database.tsx
@@ -1,12 +1,28 @@
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 
+// 환경 변수 로드
 dotenv.config();
 
-export const db = await mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
-});
+// Azure에서 설정한 연결 문자열 환경 변수 사용
+const connectionString = process.env.AZURE_MYSQL_CONNECTIONSTRING;
+
+if (connectionString) {
+  const { Database, Server, UserId, Password } = connectionString.split(';').reduce((acc, pair) => {
+    const [key, value] = pair.split('=');
+    acc[key] = value;
+    return acc;
+  }, {} as { [key: string]: string });
+
+  // MySQL 연결 설정
+  const db = await mysql.createConnection({
+    host: Server,
+    user: UserId,
+    password: Password,
+    database: Database,
+  });
+
+  console.log('MySQL connected successfully!');
+} else {
+  console.error('Connection string not found!');
+}
