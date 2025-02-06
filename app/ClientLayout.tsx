8f01ba0016434e73ec4bbf5d795ed211543d2db9
Myusol <qoralth25@gmail.com>
2025-02-06 10:45:10 +0900

    LocalStorage 일단 롤백

--- app/ClientLayout.tsx
+++ app/ClientLayout.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useEffect } from "react";
 import { usePathname } from "next/navigation";
 import Nav from "./component/Nav/Nav";
 import Footer from "./component/Footer/Footer";
@@ -9,18 +8,6 @@ export default function ClientLayout({ children }: { children: React.ReactNode }
   const pathname = usePathname();
   const isStandalonePage = pathname === "/privacy" || pathname === "/terms";
   const isMain = pathname === "/";
-
-  useEffect(() => {
-    // 창이 닫힐 때 localStorage 데이터 삭제
-    const handleUnload = () => {
-      localStorage.clear();
-    };
-
-    window.addEventListener("beforeunload", handleUnload);
-    return () => {
-      window.removeEventListener("beforeunload", handleUnload);
-    };
-  }, []);
 
   return (
     <div className="layout-container">
